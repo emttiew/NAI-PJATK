b0f2c4b6b35e662cbc5a8c30f9ffb185ee99bafd
emttiew <emttiew@gmail.com>
2020-12-04 22:38:39 +0100

    delete reduntand spaces

--- FuzzyLogicApartmentPricing/apartment_pricing.py
+++ FuzzyLogicApartmentPricing/apartment_pricing.py
@@ -23,20 +23,17 @@ year_of_construction = ctrl.Antecedent(np.arange(1945, 2021, 1), 'year_of_constr
 
 cost_per_square_meter = ctrl.Consequent(np.arange(0, 14000, 1), 'cost_per_square_meter')
 
-
 # Auto-membership function population
 
 standard.automf(3)
 location.automf(3)
 year_of_construction.automf(3)
 
-
 # Custom membership functions
 
 cost_per_square_meter['low'] = fuzz.trimf(cost_per_square_meter.universe, [0, 0, 7500])
 cost_per_square_meter['medium'] = fuzz.trimf(cost_per_square_meter.universe, [0, 7500, 14000])
 cost_per_square_meter['high'] = fuzz.trimf(cost_per_square_meter.universe, [7500, 14000, 14000])
-
 
 # Fuzzy relationship between input and output variables
 
@@ -63,13 +60,11 @@ rule3 = ctrl.Rule(antecedent=((standard['good'] & location['good'] & year_of_con
 apartment_pricing_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
 apartment_pricing = ctrl.ControlSystemSimulation(apartment_pricing_ctrl)
 
-
 # Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
 
 apartment_pricing.input['standard'] = 1
 apartment_pricing.input['location'] = 6
 apartment_pricing.input['year_of_construction'] = 2015
-
 
 # Crunch the numbers
 
